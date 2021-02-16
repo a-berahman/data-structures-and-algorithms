f919dd18fccd35f5030bc426c3d8489c8c19bb47
Ahmad Berahman <62759025+a-berahman@users.noreply.github.com>
2021-02-16 09:57:58 +0330

    adding rotate array

---
+++ array/rotateArray.go
@@ -0,0 +1,25 @@
+package array
+
+// LeetCode
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+// ex:
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+func rotate(nums []int, k int) {
+	k = k % len(nums)
+
+	reverse(nums, 0, len(nums)-1)
+	reverse(nums, 0, k-1)
+	reverse(nums, k, len(nums)-1)
+}
+func reverse(nums []int, i, j int) {
+	for i <= j {
+		nums[i], nums[j] = nums[j], nums[i]
+		j--
+		i++
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/a-berahman/data-structures-and-algorithms/sort"
+)
+
+func main() {
+	fmt.Println(sort.QucikSortTwo([]int{0, 2, 3, 5, 1, 6, 14, 15, 19, 18, 0, 99}))
 }
