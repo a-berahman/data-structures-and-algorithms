8d0b3fc834f1c034999fe858e74a994ea5e02d84
Ahmad Berahman <62759025+a-berahman@users.noreply.github.com>
2021-03-31 09:16:08 +0430

    add all premutation challenge

---
+++ string/allPermutations.go
@@ -0,0 +1,17 @@
+package string
+
+import "fmt"
+
+func GenerateAllPermutations(val []rune, left, right int) {
+	if left == right {
+		fmt.Println(string(val))
+	} else {
+		for i := left; i <= right; i++ {
+			val[left], val[i] = val[i], val[left]
+			GenerateAllPermutations(val, left+1, right)
+			val[left], val[i] = val[i], val[left]
+
+		}
+	}
+
+}
